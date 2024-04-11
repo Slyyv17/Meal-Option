7a338196bf7c566b8ea41db0c0df79b1f2626bc0
Slyyv17 <ememevictor08@gmail.com>
2024-04-11 14:24:25 -0700

    proper push

---
+++ WholeWellBeing.js
@@ -0,0 +1,7 @@
+// Navigation bar, it allows this to be toggled.
+const navToggle = document.querySelector(".navbar_toggle");
+const links = document.querySelector(".main_nav");
+
+navToggle.addEventListener('click', function () {
+    links.classList.toggle("show_nav");
+})
